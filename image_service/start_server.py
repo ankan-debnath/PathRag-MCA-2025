887e4aa40721b0176e5a71e8b057f923b3c08806
Ankan Debnath <ankan_debnath@epam.com>
2025-06-05 00:45:48 +0530

    added start_server.py script, added dependencies

---
+++ image_service/start_server.py
@@ -0,0 +1,28 @@
+import os
+
+import nest_asyncio
+from pyngrok import ngrok
+import threading
+import subprocess
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AUTH_TOKEN = os.getenv("NGROCK_AUTH_TOKEN")
+
+nest_asyncio.apply()
+ngrok.set_auth_token(AUTH_TOKEN)
+
+def run_gunicorn():
+    # Run gunicorn for your Flask app
+    subprocess.run([
+        "gunicorn",
+        "backend.app:app",
+        "--bind", "0.0.0.0:5000"
+    ])
+
+threading.Thread(target=run_gunicorn, daemon=True).start()
+
+# Start ngrok tunnel to port 5000
+public_url = ngrok.connect(5000)
+print("🔗 Public URL:", public_url)
