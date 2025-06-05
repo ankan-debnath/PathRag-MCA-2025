c0c7437e8a2ec66f80ef20a6d735b2743b352d67
Ankan Debnath <ankan_debnath@epam.com>
2025-06-05 14:36:33 +0530

    added logging in med_model_service

--- med_model_service/backend/app.py
+++ med_model_service/backend/app.py
@@ -21,23 +21,23 @@ def chatbot():
     input_ids, attention_mask = med_model.create_conversations(message)
     print(request.files)
 
-    if 'image' in request.files:
-        files = request.files.getlist("image")
+    # if 'image' in request.files:
+    #     files = request.files.getlist("image")
+    #
+    #     for idx, file in enumerate(files):
+    #         filename = f"image_{idx}.png"
+    #         filepath = os.path.join(UPLOAD_FOLDER, filename)
+    #         file.save(filepath)
+    #
+    #         image_path = os.path.join(UPLOAD_FOLDER, filepath)
+    #         image_tensor = med_model.get_image_tensors(image_path)
+    #         answer = med_model.generate_response(input_ids, attention_mask, image_tensor)
+    #         med_responces.append({ filename : answer })
+    # else:
+    #     answer = med_model.generate_response(input_ids, attention_mask)
+    #     med_responces.append(answer)
 
-        for idx, file in enumerate(files):
-            filename = f"image_{idx}.png"
-            filepath = os.path.join(UPLOAD_FOLDER, filename)
-            file.save(filepath)
-
-            image_path = os.path.join(UPLOAD_FOLDER, filepath)
-            image_tensor = med_model.get_image_tensors(image_path)
-            answer = med_model.generate_response(input_ids, attention_mask, image_tensor)
-            med_responces.append({ filename : answer })
-    else:
-        answer = med_model.generate_response(input_ids, attention_mask)
-        med_responces.append(answer)
-
-    return jsonify( { 'response' : med_responces } )
+    return jsonify( { 'response' : "med_responces" } )
 
 @app.route("/", methods=["GET"])
 def index():
